05dc77e100e6c362549c70f202868cd17a028b8d
ewen_ <ewenhenderson@gmail.com>
2017-10-31 21:32:14 +0000

    refine rotate pair fun, add rotate all function

--- lp_do.R
+++ lp_do.R
@@ -12,3 +12,18 @@ source('lp_funcs.R')
 
 # load player data
 player_data <- read_csv("player_data.csv")
+
+
+# CLEAN -------------------------------------------------------------------
+
+# create goalies df
+goalie_data <- filter(player_data, position=="Goalkeeper")
+
+# unique goalies list
+unique_goalies <- distinct(goalie_data, player_id, web_name, team_name)
+
+
+# DO ----------------------------------------------------------------------
+
+# loop rotation function test
+foo <- lapply(goalie_data, function(x) rotate_pts())
--- lp_funcs.R
+++ lp_funcs.R
@@ -1,4 +1,5 @@
-rotate_pts <- function(player_id_a, player_id_b) {
+# function to get points total if two named players rotated
+rotate_pair <- function(player_id_a, player_id_b) {
   
 # player ids
 player_ids <- c(player_id_a, player_id_b)
@@ -21,3 +22,57 @@ max_points_total <- sum(max_round_points$best_points)
 return(max_points_total)
 
 }
+
+# function to get points total for all rotations with a named player
+rotate_all <- function(id_player) {
+  
+  # player points
+  player_points <- player_data %>%
+    # match player id to inputs
+    dplyr::filter(player_id == id_player) %>%
+    # keep points
+    dplyr::select(round, player_id, total_points)
+  
+  # other player points
+  other_points <- player_data %>%
+    # match player id to inputs
+    dplyr::filter(player_id != id_player) %>%
+    # keep points
+    dplyr::select(round, player_id, total_points)
+  
+  # nest other players points
+  other_points_nested <- other_points %>%
+    split(.$player_id)
+  
+  # function to total rotation points
+  collect_points <- function(x) { 
+    
+    # get max points for each round
+    x <- x %>%
+    dplyr::group_by(round) %>%
+    dplyr::summarise(best_points=max(total_points))
+    
+    # get max points total
+    max_points_total <- sum(x$best_points)
+  }
+  
+  # iterate through other players to compare points to orig player
+  points_compare <- other_points_nested %>%
+    purrr::map(~bind_rows(., player_points)) %>%
+    purrr::map(~collect_points(.))
+  
+  # create df from named list
+  points_compare <- stack(points_compare)
+  
+  # set colnames
+  points_compare <- points_compare %>%
+    dplyr::mutate(player_id=id_player) %>%
+    dplyr::select(player_id, rotation_player_id=ind, points=values)
+  
+  # convert factor to numeric
+  points_compare$rotation_player_id <- as.numeric(as.character(points_compare$rotation_player_id))
+  
+  return(points_compare)
+  
+}
+
