2cd091e270cc49b2726cafa142b7e463e96b0ea1
ewen_ <ewenhenderson@gmail.com>
2017-10-31 22:24:03 +0000

    create goalie rotation df

--- lp_do.R
+++ lp_do.R
@@ -31,4 +31,17 @@ test <- lapply(seq_along(goalie_data$player_id), rotate_all, data=goalie_data)
 # bind these items
 goalie_rotation <- bind_rows(test) %>%
   # get unique goalie combos
-  
+  mutate(key = paste0(pmin(player_id, rotation_player_id), pmax(player_id, rotation_player_id), 
+                      sep = "")) %>% 
+  distinct(key, .keep_all = TRUE) %>%
+  select(-key)
+
+# join metadata
+goalie_rotation <- goalie_rotation %>%
+  # join original player
+  left_join(unique_goalies, by="player_id") %>%
+  # join rotation pplayer
+  left_join(unique_goalies, by=c("rotation_player_id"="player_id")) %>%
+  # rename cols
+  rename(player_name=web_name.x, team_name=team_name.x, rotation_player_name=web_name.y,
+         rotation_team_name=team_name.y)
