b1c1c75f1a5cf349a4916329861d7f2bd25a5704
ewen_ <ewenhenderson@gmail.com>
2017-10-31 23:03:02 +0000

    iteration id fix

--- lp_do.R
+++ lp_do.R
@@ -3,6 +3,7 @@
 
 # load pkgs
 library(tidyverse)
+library(glue)
 
 # load funcs
 source('lp_funcs.R')
@@ -17,19 +18,23 @@ player_data <- read_csv("player_data.csv")
 # CLEAN -------------------------------------------------------------------
 
 # create goalies df
-goalie_data <- filter(player_data, position=="Goalkeeper")
+goalie_data <- filter(player_data, position=="Goalkeeper") %>%
+  # add 'start price' col
+  group_by(player_id) %>%
+  mutate(start_price=first(price)) %>%
+  ungroup()
 
 # unique goalies list
-unique_goalies <- distinct(goalie_data, player_id, web_name, team_name)
+unique_goalies <- distinct(goalie_data, player_id, web_name, team_name, start_price)
 
 
 # DO ----------------------------------------------------------------------
 
 # get points rotation for all goalies
-test <- lapply(seq_along(goalie_data$player_id), rotate_all, data=goalie_data)
+goalie_rotation <- lapply(unique(goalie_data$player_id), rotate_all, data=goalie_data)
 
 # bind these items
-goalie_rotation <- bind_rows(test) %>%
+goalie_rotation <- bind_rows(goalie_rotation) %>%
   # get unique goalie combos
   mutate(key = paste0(pmin(player_id, rotation_player_id), pmax(player_id, rotation_player_id), 
                       sep = "")) %>% 
@@ -43,5 +48,17 @@ goalie_rotation <- goalie_rotation %>%
   # join rotation pplayer
   left_join(unique_goalies, by=c("rotation_player_id"="player_id")) %>%
   # rename cols
-  rename(player_name=web_name.x, team_name=team_name.x, rotation_player_name=web_name.y,
-         rotation_team_name=team_name.y)
+  rename(player_name=web_name.x, team_name=team_name.x, start_price=start_price.x,
+         rotation_player_name=web_name.y, rotation_team_name=team_name.y, 
+         rotation_start_price=start_price.y) %>%
+  # add total value col and name col
+  mutate(total_price=start_price+rotation_start_price,
+         combo=glue("{player_name} ({team_name}) & {rotation_player_name} ({rotation_team_name}) "))
+
+
+# VIZ ---------------------------------------------------------------------
+
+# top ten combos
+goalie_rotation %>%
+  ggplot(aes(x=points, y=total_price)) +
+  geom_jitter()
